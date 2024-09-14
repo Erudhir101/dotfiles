21d856c173054c4ef93c85151fb170c8375f9203
erudhir101 <bieusc@hotmail.com>
2024-09-13 21:48:05 -0300

    change background wallpaper

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 autorandr --change &
-feh --bg-scale ~/.config/wallpapers/'red sunset'.png &
+feh --bg-scale /mnt/Backup/Documents/Images/painting.jpg &
 picom -b &
 libinput-gestures-setup start &
 blueman-applet &
