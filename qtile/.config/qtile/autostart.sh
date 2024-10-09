bad0cb16c2b053c9c115f61c19000563dd6808e0
erudhir101 <bieusc@hotmail.com>
2024-10-09 00:13:33 -0300

    change i3 feh config and rofi icons

--- qtile/.config/qtile/autostart.sh
+++ qtile/.config/qtile/autostart.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-autorandr --change &
-feh --bg-scale /mnt/Backup/Documents/Images/painting.jpg &
+# autorandr --change &
+feh --bg-scale /media/erudhir/Backup/Documents/Images/painting.jpg &
 picom -b &
-libinput-gestures-setup start &
-blueman-applet &
-nm-applet &
-flameshot & 
+# libinput-gestures-setup start &
+# blueman-applet &
+# nm-applet &
+# flameshot & 
