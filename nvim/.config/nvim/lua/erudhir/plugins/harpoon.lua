a313f638e43493015dbe0bc93efcb2d5f446a3e6
erudhir101 <bieusc@hotmail.com>
2024-12-26 10:57:47 -0300

    add neovim plugin "harpoon"

---
+++ nvim/.config/nvim/lua/erudhir/plugins/harpoon.lua
@@ -0,0 +1,35 @@
+return {
+	"ThePrimeagen/harpoon",
+	branch = "harpoon2",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	config = function()
+		local status, harpoon = pcall(require, "harpoon")
+		if not status then
+			return
+		end
+
+		harpoon.setup({})
+
+		vim.keymap.set("n", "<leader>a", function()
+			harpoon:list():add()
+		end, { desc = "add file to harpoon" })
+		vim.keymap.set("n", "<C-e>", function()
+			harpoon.ui:toggle_quick_menu(harpoon:list())
+		end, { desc = "toggle harpoon" })
+
+		-- make keybinding for change with all numbers base on position
+		for i = 1, 9 do
+			vim.keymap.set("n", string.format("<C-%d>", i), function()
+				harpoon:list():select(i)
+			end, { desc = string.format("select the %d file", i) })
+		end
+
+		-- Toggle previous & next buffers stored within Harpoon list
+		vim.keymap.set("n", "<S-Tab>", function()
+			harpoon:list():prev()
+		end, { desc = "go to the prev in the list" })
+		vim.keymap.set("n", "<Tab>", function()
+			harpoon:list():next()
+		end, { desc = "go to the next in the list" })
+	end,
+}
